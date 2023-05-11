9504ea46c4563b5d5a76a6102429187b60a05624
VaNomad <sjur.hassel@gmail.com>
2023-05-11 14:05:09 +0100

    changes extend img to mushrooms

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,14 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        beetleYellow:
-          "url('https://images.unsplash.com/photo-1565520651265-1148c3b277f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80')",
-        beetleBlue:
-          "url('https://images.unsplash.com/photo-1531850959096-cfbb6f26c5a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dnclMjBiZWV0bGV8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60')",
-        beetleCamping:
-          "url('https://images.unsplash.com/photo-1472806679307-eab7abd1eb32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fHZ3JTIwYmVldGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60')",
-        beetleBeauty:
-          "url('https://images.unsplash.com/photo-1633683536118-ed815cd3dd17?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA0fHx2dyUyMGJlZXRsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60')",
+        whiteButton:
+          "url('https://www.farmersalmanac.com/wp-content/uploads/2021/11/button-mushrooms_as105282404-950x633.jpeg')",
+        crimini:
+          "url('https://www.farmersalmanac.com/wp-content/uploads/2021/11/cremini-mushrooms_as190119810-950x633.jpeg')",
+        shiitake:
+          "url('https://www.farmersalmanac.com/wp-content/uploads/2021/11/shiitake-mushrooms-as197629491-950x634.jpeg')",
+        enoki:
+          "url('https://images.pexels.com/photos/5987623/pexels-photo-5987623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
         beetleLife:
           "url('https://images.unsplash.com/photo-1603208226824-8b1f8537f57f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTQzfHx2dyUyMGJlZXRsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60')",
         myAvatar: "url('./src/assets/CIMG0065.jpg')",
