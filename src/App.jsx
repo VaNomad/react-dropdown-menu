5e499a1bc180ac85187a71cac29b88fe9077c82d
VaNomad <sjur.hassel@gmail.com>
2023-05-11 12:15:35 +0100

    Adds cards w/custom bg + utility classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className='text-6xl font-bold tracking-widest'>Hello There</div>
-  )
+    <div className="bg-[#130114] h-screen">
+      <div className="flex flex-col justify-between items-center p-6">
+        <div className="text-white text-5xl font-bold tracking-widest ">
+          <p className="">My Garage</p>
+          <div className="bg-myAvatar card-zoom-avatar"></div>
+        </div>
+        <div className="flex flex-wrap justify-center">
+          <div className="card-zoom">
+            <div className="card-zoom-image bg-beetleYellow"></div>
+            <h1 className="card-zoom-text mb-[80px]">VW</h1>
+            <h1 className="card-zoom-text mt-[70px]">LOVE</h1>
+          </div>
+          <div className="card-zoom">
+            <div className="card-zoom-image bg-beetleCamping"></div>
+            <h1 className="card-zoom-text mb-[80px]">VW</h1>
+            <h1 className="card-zoom-text mt-[70px]">CAMPING</h1>
+          </div>
+          <div className="card-zoom">
+            <div className="card-zoom-image bg-beetleBeauty"></div>
+            <h1 className="card-zoom-text mb-[80px]">MY</h1>
+            <h1 className="card-zoom-text mt-[70px]">LIFE</h1>
+          </div>
+          <div className="card-zoom">
+            <div className="card-zoom-image bg-beetleLife"></div>
+            <h1 className="card-zoom-text mb-[80px]">vw</h1>
+            <h1 className="card-zoom-text mt-[70px]">1300</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        beetleYellow:
+          "url('https://images.unsplash.com/photo-1565520651265-1148c3b277f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80')",
+        beetleBlue:
+          "url('https://images.unsplash.com/photo-1531850959096-cfbb6f26c5a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dnclMjBiZWV0bGV8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60')",
+        beetleCamping:
+          "url('https://images.unsplash.com/photo-1472806679307-eab7abd1eb32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fHZ3JTIwYmVldGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60')",
+        beetleBeauty:
+          "url('https://images.unsplash.com/photo-1633683536118-ed815cd3dd17?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA0fHx2dyUyMGJlZXRsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60')",
+        beetleLife:
+          "url('https://images.unsplash.com/photo-1603208226824-8b1f8537f57f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTQzfHx2dyUyMGJlZXRsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60')",
+        myAvatar: "url('./src/assets/CIMG0065.jpg')",
+      },
+    },
   },
   plugins: [],
 };
