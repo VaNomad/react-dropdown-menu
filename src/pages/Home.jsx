74957851e74ece6423d896cb5b0f97d041a12e36
VaNomad <sjur.hassel@gmail.com>
2023-05-12 14:26:27 +0100

    Moves Cards to Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,32 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="bg-[#130114] h-screen">
+
+      {/* Container */}
+      <div className="flex flex-col p-6">
+
+        {/* Cards */}
+        <div className="flex flex-wrap justify-center">
+          <div className="card-zoom">
+            <div className="card-zoom-image bg-whiteButton"></div>
+            <h1 className="card-zoom-text mb-[80px]">WHITE</h1>
+            <h1 className="card-zoom-text mt-[70px]">BUTTONS</h1>
+          </div>
+          <div className="card-zoom">
+            <div className="card-zoom-image bg-crimini"></div>
+            <h1 className="card-zoom-text">CRIMINI</h1>
+          </div>
+          <div className="card-zoom">
+            <div className="card-zoom-image bg-shiitake"></div>
+            <h1 className="card-zoom-text">SHIITAKE</h1>
+          </div>
+          <div className="card-zoom">
+            <div className="card-zoom-image bg-enoki scale-[135%]"></div>
+            <h1 className="card-zoom-text">ENOKI</h1>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
